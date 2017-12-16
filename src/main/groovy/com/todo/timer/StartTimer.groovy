d76b3acb7c9c5b41cb2fde4ed76d1449ab958881
iamAkash <akash@nexthoughts.com>
2017-12-16 16:47:48 +0530

    Add Corn Job

--- src/main/groovy/com/todo/timer/StartTimer.groovy
+++ src/main/groovy/com/todo/timer/StartTimer.groovy
@@ -1,7 +1,10 @@
 package com.todo.timer
 
+import com.diabolicallabs.vertx.cron.CronObservable
 import com.todo.mail.SendEmail
 import io.vertx.core.Vertx
+import io.vertx.rx.java.RxHelper
+import rx.Scheduler
 
 class StartTimer {
     public static void main(String[] args) {
@@ -10,11 +13,11 @@ class StartTimer {
 
     void executeTimer() {
         Vertx vertx = Vertx.vertx()
-        vertx.setTimer(1000, { id ->
+        Scheduler scheduler = RxHelper.scheduler(vertx);
+        CronObservable.cronspec(scheduler, "0 0/1 * * * ?", "IST")
+                .subscribe({
+            println("@@@@@@@@@@@@@Triger@@@@@@@@@@@@@")
             SendEmail.triggerNow("anubhav@fintechlabs.in", "iiiiiiii", "TESTING HELLO", vertx)
-        })
-        vertx.setPeriodic(1000 * 60 * 60 * 24, { id ->
-            SendEmail.triggerNow("anubhav@fintechlabs.in", "iiiiiiii", "TESTING HELLO", vertx)
-        })
+        });
     }
 }
