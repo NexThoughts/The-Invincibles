0ae268e636db3a8a4ea48ee0c81c4813d61f53b7
Karan Arora <karan@fintechlabs.in>
2017-12-16 12:27:31 +0530

    added gson dependency and basic classes

---
+++ src/main/groovy/com/bo/UserBO.groovy
@@ -0,0 +1,9 @@
+package com.bo
+
+/**
+ * Created by karan on 16/12/17.
+ */
+class UserBO {
+
+
+}
---
+++ src/main/groovy/com/util/AppUtil.groovy
@@ -0,0 +1,9 @@
+package com.util
+
+/**
+ * Created by karan on 16/12/17.
+ */
+class AppUtil {
+
+
+}
